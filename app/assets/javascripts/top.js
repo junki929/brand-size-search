0a1cf05655366f7baa5e59223b5155e94b6a6b39
toshima66 <toshima66@6vox.com>
2014-02-21 01:35:14 +0900

    "フィルター検索を実装する #4"
    Railsと画面のインターフェース部分も修正
    routesなども変更している

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,25 +1,54 @@
 ;(function ( $, window, document, undefined ) {
 $(function(){
 
-$('.btn').on('click', function(){
-  //ここでJSON取得
-  //ex. $.getJSON('',function(){ /* ここに取得できた場合の処理を書く */ });
-});
+var $brandTmp = $('.div-brand').first().clone();
+$('.div-brand').first().remove();
 
-$('#brand_name').on('keyup', function(){
-/*
-  filter 処理
-  ex. 
-  var re = new RegExp(this.data('name'), 'i');
-  $('.li_brand_size').each(function(){
-    if (テキスト.match(re)) {
-      $(this).fadeIn();
+var bsfilter = function(){
+  $bsname = $('#brand_name');
+  var re = new RegExp($bsname.val(), 'i');
+  var mh = 150;
+  $('.div-brand').each(function(){
+    var $this = $(this);
+    if ($this.data('bs_name').match(re)) {
+      $this.fadeIn();
+      mh = Math.max(mh, $this.height());
     } else {
-      $(this).fadeOut();
+      $this.fadeOut();
     }
   });
-*/
+  $('.div-brand:visible').height(mh);
+};
+
+$('.btn').on('click', function(){
+  var after = function(){
+    var $results = $('#results');
+    $results.empty();
+    var sizes = $(':checked').map(function(){ return this.value }).get().join('_')
+    if (!sizes) { return false; }
+    //console.log('get: '+'/search/'+sizes);
+    $.getJSON('/search/'+sizes, function(data){
+      //console.log(data);
+      if (!data) {return false;}
+
+      for (var i=0; i<data.length; i++) {
+        var bs = data[i];
+        var $tmp = $brandTmp.clone();
+        $tmp.data('bs_name', bs.name);
+        for (var pname in bs) {
+          //console.log('pname: '+pname);
+          //console.log('.bs_'+pname+', '+bs[pname]);
+          $tmp.find('.bs_'+pname).text(bs[pname]);
+        }
+        $tmp.appendTo($results);
+      }
+      bsfilter();
+    });
+  };
+  setTimeout(after, 100);
 });
+
+$('#brand_name').on('keyup', bsfilter);
 
 });
 })(jQuery, window, document);
