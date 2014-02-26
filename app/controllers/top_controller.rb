5a98c365df2305a06f7c81f517032ec438defcec
junki929 <junki.nishizaki@6vox.com>
2014-02-26 14:18:34 +0900

    #14 アイテムごとのサイズ検索時の検索機能 微修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,21 +2,21 @@
 
 class TopController < ApplicationController
   def search
-    select_tops = params[:tops_sizes].presence
+    select_tops = params[:tops].presence
     select_tops  = select_tops ? select_tops.upcase.split('_') : []
     data_tops = select_tops.blank? ? [] : BrandSize.select_item_and_sizes('Tops',select_tops)
 
 
-    select_shirts = params[:shirts_sizes].presence
+    select_shirts = params[:shirts].presence
     select_shirts  = select_shirts ? select_shirts.upcase.split('_') : []
     data_shirts = select_shirts.blank? ? [] : BrandSize.select_item_and_sizes('Shirts',select_shirts)
 
-    select_bottoms = params[:bottoms_sizes].presence
+    select_bottoms = params[:bottoms].presence
     select_bottoms  = select_bottoms ? select_bottoms.upcase.split('_') : []
     data_bottoms = select_bottoms.blank? ? [] : BrandSize.select_item_and_sizes('Bottoms',select_bottoms)
 
 
-    select_shoes = params[:shoes_sizes].presence
+    select_shoes = params[:shoes].presence
     select_shoes  = select_shoes ? select_shoes.upcase.split('_') : []
     data_shoes = select_shoes.blank? ? [] : BrandSize.select_item_and_sizes('Shoes',select_shoes)
 
